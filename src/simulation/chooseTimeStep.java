7ea4720c9b5ea67c6ec73dd0bb8cfbfd1048ccd1
sudesh <sudeshkagrawal@users.noreply.github.com>
2020-09-03 20:06:40 -0500

    Added a method to choose time step for TN1PC spread

---
+++ src/simulation/chooseTimeStep.java
@@ -0,0 +1,144 @@
+package simulation;
+
+import dataTypes.parameters;
+import network.graph;
+import org.jgrapht.Graphs;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+/**
+ * Simulation runs to choose appropriate time step for a given spread model and parameters.
+ * @author Sudesh Agrawal (sudesh@utexas.edu).
+ * Last Updated: September 3, 2020.
+ */
+public class chooseTimeStep
+{
+	/**
+	 * A map from a 7-tuple to the simulation runs.
+	 * 7-tuple: (model (TN11C, RAEPC, etc.), network name, time step, repetitions,
+	 * false negative probability, transmissability (p), number of honeypots).
+	 * 7-tuple is represented through {@code parameters} class.
+	 */
+	Map<parameters, List<List<Integer>>> mapParametersToSimulationRuns;
+	/**
+	 * A map from a 7-tuple to the time steps required to get a given percentage infection.
+	 * 7-tuple: (model (TN11C, RAEPC, etc.), network name, time step, repetitions,
+	 * false negative probability, transmissability (p), number of honeypots).
+	 * 7-tuple is represented through {@code parameters} class.
+	 * Each element in the list is the time steps for a simulation repetition.
+	 */
+	Map<parameters, List<Integer>> mapParametersToTimeForInfection;
+	
+	/**
+	 * Constructor.
+	 *
+	 * @param mapParametersToSimulationRuns a map from {@code parameters} to the simulation runs
+	 * @param mapParametersToTimeForInfection A map from {@code parameters} to
+	 *                                        the time steps required to get a given percentage infection.
+	 */
+	public chooseTimeStep(Map<parameters, List<List<Integer>>> mapParametersToSimulationRuns,
+	                      Map<parameters, List<Integer>> mapParametersToTimeForInfection)
+	{
+		this.mapParametersToSimulationRuns = mapParametersToSimulationRuns;
+		this.mapParametersToTimeForInfection = mapParametersToTimeForInfection;
+	}
+	
+	/**
+	 * Constructor.
+	 */
+	public chooseTimeStep()
+	{
+		this(new HashMap<>(), new HashMap<>());
+	}
+	
+	/**
+	 * Getter.
+	 *
+	 * @return returns {@code mapParametersToSimulationRuns}.
+	 */
+	public Map<parameters, List<List<Integer>>> getMapParametersToSimulationRuns()
+	{
+		return mapParametersToSimulationRuns;
+	}
+	
+	/**
+	 * Getter.
+	 *
+	 * @return returns {@code mapParametersToTimeForInfection}.
+	 */
+	public Map<parameters, List<Integer>> getMapParametersToTimeForInfection()
+	{
+		return mapParametersToTimeForInfection;
+	}
+	
+	public void TN1PCSimulationRuns(graph g, List<parameters> params, int[] seed) throws Exception
+	{
+		// Remove self-loops if any from the graph
+		System.out.println("Network has "+g.getG().vertexSet().size()
+				+" nodes and "+g.getG().edgeSet().size()+" edges.");
+		g.removeSelfLoops();
+		System.out.print("Removed self-loops (if any) from the graph: ");
+		final int n = g.getG().vertexSet().size();
+		System.out.println("(new) network has "+n+" nodes and "+g.getG().edgeSet().size()+" edges.");
+		
+		if ((seed.length!=2) && (seed.length!=3))
+			throw new Exception("Seed array should be of length 3!");
+		
+		System.out.println("Starting simulation runs...");
+		for(parameters param : params)
+		{
+			System.out.println("\t "+param.toString());
+			double pi = param.getPercentInfection();
+			int rep = param.getNumberOfSimulationRepetitions();
+			double p = param.getTransmissability();
+			int numberOfNodesInfected = (int) Math.floor(pi*n*0.01);
+			System.out.println("Number of nodes to be infected = "+numberOfNodesInfected);
+			
+			SplittableRandom initialLocationGenChoice =
+					new SplittableRandom(seed[0]+Double.doubleToLongBits(pi)+rep+Double.doubleToLongBits(p));
+			SplittableRandom neighborGenChoice =
+					new SplittableRandom(seed[1]+Double.doubleToLongBits(pi)+rep+Double.doubleToLongBits(p));
+			SplittableRandom transmissableGen =
+					new SplittableRandom(seed[2]+Double.doubleToLongBits(pi)+rep+Double.doubleToLongBits(p));
+			//int[] initialLocationRuns = getInitialLocationRuns(g, initialLocationGenChoice, rep);
+			List<Integer> nodes = new ArrayList<>(g.getG().vertexSet());
+			int[] initialLocationRuns = IntStream.range(0, rep)
+										.map(i -> nodes.get(initialLocationGenChoice.nextInt(0, n))).toArray();
+			
+			List<Integer> t0Runs = new ArrayList<>(rep);
+			for (int x=0; x<rep; x++)
+			{
+				System.out.println("\t\t Simulation run "+(x+1));
+				int initialLocation = initialLocationRuns[x];
+				System.out.println("\t\t Initial location of virus: "+initialLocation);
+				Set<Integer> infected = new HashSet<>(numberOfNodesInfected);
+				
+				// time step 0
+				int currentInfected = initialLocation;
+				infected.add(currentInfected);
+				
+				
+				int t=1;
+				while (infected.size()<numberOfNodesInfected)
+				{
+					System.out.println("\t\t\t Time: "+t);
+					double transmissable = transmissableGen.nextDouble(0, 1);
+					if (transmissable<=p)
+					{
+						List<Integer> currentNeighbors = Graphs.neighborListOf(g.getG(), currentInfected);
+						currentInfected = currentNeighbors.get(neighborGenChoice.nextInt(currentNeighbors.size()));
+						infected.add(currentInfected);
+						System.out.println("\t\t\t\t Current infected node: "+currentInfected);
+					}
+					t++;
+				}
+				t0Runs.add(t-1);
+				System.out.println("\t\t Infected nodes: "+infected.toString());
+			}
+			System.out.println("\t Infection times: "+t0Runs);
+			mapParametersToTimeForInfection.put(param, t0Runs);
+		}
+		System.out.println("Ending simulation runs...");
+	}
+}
